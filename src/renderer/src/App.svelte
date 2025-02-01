76ff09de86f4eeb3c5ffaef1f19ec42dc21841b9
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2025-02-01 12:03:45 +0100

    first commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Versions from './components/Versions.svelte'
+  import electronLogo from './assets/electron.svg'
+
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<img alt="logo" class="logo" src={electronLogo} />
+<div class="creator">Powered by electron-vite</div>
+<div class="text">
+  Welcome to
+  <span class="svelte">Pulse</span>
+</div>
+<Versions />
